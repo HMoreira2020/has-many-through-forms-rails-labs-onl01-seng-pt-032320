3fd91d236a5f6710dd1cd43a6cee19f3fa6f0cfa
HMoreira2020 <moreiraheather@gmail.com>
2020-07-29 14:27:17 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,16 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories, reject_if: :all_blank
+
+  def comments_content=(comment_contents)
+    comment_contents.values.each do |comment_content|
+      comment = Comment.find_or_create_by(comment_content)
+      self.comments << comment  
+    end 
+  end 
+
+
 
 
 end
